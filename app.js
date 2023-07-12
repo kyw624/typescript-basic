838bc384fe048295613470c2828f2e8024a241f0
Y.W KIM <kyw624@gmail.com>
2023-07-12 16:29:40 +0900

    Add: Initial Commit

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
-var person = {
-    name: 'Youngwoo',
-    age: 28,
-};
-console.log(person.name);
+////
+// unknown type
+var userInput;
+var userName;
+userInput = 5;
+userInput = 'Max';
+// userName = userInput; // 에러. userInput이 any타입이면 에러없음.
+// unknown은 해당 변수에 저장된 타입을 확인해야 할당 가능.
+if (typeof userInput === 'string') {
+    userName = userInput; // 정상작동
+}
+////
+// never type
+function generateError(message, code) {
+    throw {
+        message: message,
+        errorCode: code,
+    };
+}
+generateError('에러 발생!', 500);
--- app.ts
+++
@@ -1,9 +0,0 @@
-const person: {
-  name: string;
-  age: number;
-} = {
-  name: 'Youngwoo',
-  age: 28,
-};
-
-console.log(person.name);
