672ebc86069cf324ce4baa294b85678f953ae124
Y.W KIM <kyw624@gmail.com>
2024-06-18 01:30:16 +0900

    Feat: Google Maps API를 활용해 지도 검색 기능 구현

--- practice/map/src/app.ts
+++ practice/map/src/app.ts
@@ -1,1 +1,48 @@
-// Code goes here!
+import axios from 'axios';
+import { API_KEY } from './api';
+
+const GOOGLE_API_KEY = API_KEY;
+
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: 'OK' | 'ZERO_RESULTS';
+};
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+
+  const enteredAddress = addressInput.value;
+
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${GOOGLE_API_KEY}`
+    )
+    .then((response) => {
+      if (response.data.status !== 'OK') {
+        throw new Error('Could not fetch location!');
+      }
+
+      const coordinates = response.data.results[0].geometry.location;
+      const map = new google.maps.Map(document.getElementById('map')!, {
+        center: coordinates,
+        zoom: 18,
+      });
+
+      new google.maps.Marker({
+        map: map,
+        position: coordinates,
+        title: 'Uluru',
+      });
+    })
+    .catch((error) => {
+      alert(error.message);
+      console.log(error);
+    });
+}
+
+form.addEventListener('submit', searchAddressHandler);
